1f07669bc0f1e0326807d94359056b0faeecd26c
unknown <aadilsyed375@gmail.com>
2026-06-28 20:22:36 +0530

    Navbar Added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
-
-export default function Home() {
+export default function HomePage() {
   return (
-    <>
-      <h1>E-Commerce Website</h1>
-    </>
+    <main className="mx-auto max-w-7xl p-10">
+      <h1 className="text-4xl font-bold">Welcome to AI Shop</h1>
+
+      <p className="mt-5 text-gray-500">AI Powered Ecommerce Website</p>
+    </main>
   );
 }
