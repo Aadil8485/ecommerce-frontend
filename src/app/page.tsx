6c35d04f417086434b6fdce1a0159e9063483366
unknown <aadilsyed375@gmail.com>
2026-07-08 22:13:27 +0530

    add featured products

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
 import Hero from "@/src/components/home/Hero";
 import Categories from "@/src/components/home/Categories";
-import FeaturedProducts from "@/src/components/home/FeaturedProducts";
+import FeaturedProducts from "@/src/components/featured-products/FeaturedProducts";
 import FlashSale from "@/src/components/home/FlashSale";
 import NewArrivals from "@/src/components/home/NewArrivals";
 import AIRecommendations from "@/src/components/home/AIRecommendations";
